664fd2189cebd31cc8fe316cc9916579d1bf102c
Vinod Pammar <vinodpammar9@gmail.com>
2024-11-13 13:45:57 +0530

    New line Added

---
+++ 01.Arrays/createArr.java
@@ -0,0 +1,7 @@
+public class createArr {
+    public static void main(String[] args) {
+        int arr[]={1,4,2,5,4};
+        String arr1[]={"vinod","prakash","pramod","harish"};
+        int arr2[] = {41,24,54,20};
+    }
+}
